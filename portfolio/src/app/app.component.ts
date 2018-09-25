72b40848e643c89ad03c06cdf074e14e086fe367
alex <djalex12@hotmail.com>
2018-09-24 21:21:28 -0500

    se agregó el listado de los proyectos

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  projects: Project[]=[
+    {
+      name: 'Orion Mobile',
+      url: 'https://wallpapershome.com/images/pages/ico_h/11998.jpg'
+    },
+    {
+      name: 'Orion Web Site',
+      url: 'http://www.oetpo.org/opphotos/big/15/159890_mac-wallpaper-4k.jpg'
+    },
+    {
+      name: 'Orion Quotation',
+      url:'https://wallpapersultra.net/wp-content/uploads/Free-Mac-Wallpaper-Full-HD-1080p-800x450.jpg'
+    }
+  ]
+}
+
+export class Project{
+  name: string;
+  url: string;
 }
